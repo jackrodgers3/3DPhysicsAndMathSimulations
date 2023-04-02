262459a29ecde89a232d8511ad8137023c8cc602
Jack <jackprodgers03@gmail.com>
2023-04-02 09:51:17 -0400

    2var support

--- Math/Differential Equation Solver.py
+++ Math/Differential Equation Solver.py
@@ -5,10 +5,10 @@ import numpy as np
 v.scene.height = 720
 v.scene.width = 1500
 o = v.vector(0,0,0)
-t = -5
+t = -3
 deltat = 1e-2
 
-initX = 3
+initX = -3
 step = 0.3
 lim = 10
 xaxis = v.box(pos = o, size = v.vector((2*lim)+1, 0.05, 0.05), color = v.color.red)
@@ -21,8 +21,8 @@ L2 = v.label(pos = v.vector(0,0,(lim+1)),
             box = False)
 
 #define differential equation here
-def dxdt(x):
-    g = -1 * x
+def dxdt(x, t):
+    g = m.sqrt((x**2)-(m.cos(t)*(t**2)))
     return g
 
 marker = v.sphere(pos = v.vector(initX,0,t), radius = 0.1, color = v.color.yellow, make_trail = True)
@@ -31,7 +31,7 @@ marker = v.sphere(pos = v.vector(initX,0,t), radius = 0.1, color = v.color.yello
 for i in np.arange(-1*lim, lim, step):
     for j in np.arange(-1*lim, lim, step):
         try:
-            vij = v.vector(dxdt(i), 0, 1)
+            vij = v.vector(dxdt(i, j), 0, 1)
             v.arrow(pos=v.vector(i, 0, j), axis=v.hat(vij) * step, shaftwidth=step * 0.1, color=v.color.cyan)
         except ValueError as ve:
             pass
@@ -40,8 +40,9 @@ cancel = 0
 while (abs(marker.pos.x) <= lim and abs(marker.pos.z) <= lim) and cancel == 0:
     v.rate(80)
     xc = marker.pos.x
+    zc = marker.pos.z
     try:
-        xprime = v.vector(dxdt(xc), 0, 1)
+        xprime = v.vector(dxdt(xc, zc), 0, 1)
     except ValueError as ve:
         cancel = 1
     marker.pos = marker.pos + (xprime*deltat)
